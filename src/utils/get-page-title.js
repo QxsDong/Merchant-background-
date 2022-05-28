0c5bd07d76d474fe10ccd6f1be2f797b8350e162
Qu1999 <2274599030@qq.com>
2022-05-28 23:43:18 +0800

    new function

--- src/utils/get-page-title.js
+++ src/utils/get-page-title.js
@@ -1,6 +1,6 @@
 import defaultSettings from '@/settings'
 
-const title = defaultSettings.title || 'Vue Admin Template'
+const title = defaultSettings.title || 'ACH'
 
 export default function getPageTitle(pageTitle) {
   if (pageTitle) {
